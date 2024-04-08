329f6946e4d3859ee8b34c3a42999414dcbfbb1d
SBrown27 <113198958+SBrown27@users.noreply.github.com>
2024-04-07 20:14:44 -0400

    Uploading to git

--- order.php
+++ order.php
@@ -10,8 +10,18 @@
 	 		  - Execute the SQL query using the pdo function and fetch the result
 	 		  - Return the order info
 	 */
-
+	function get_order_and_info(PDO $pdo, $email, $orderNum) {
+		$sql = "SELECT *
+				FROM orders
+				JOIN customer ON orders.custnum = customer.custnum
+				WHERE customer.email = :email AND orders.ordernum = :orderNum";
 	
+		// Execute the SQL query using the pdo function and fetch the result
+		$info = pdo($pdo, $sql, ['email' => $email,'orderNum'=>$orderNum])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+		return $info;
+	}
+	
+	$orderInfo = null;
 	// Check if the request method is POST (i.e, form submitted)
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		
@@ -20,7 +30,7 @@
 
 		// Retrieve the value of the 'orderNum' field from the POST data
 		$orderNum = $_POST['orderNum'];
-
+		$orderInfo = get_order_and_info($pdo, $email, $orderNum);
 
 		/*
 		 * TO-DO: Retrieve info about order from the db using provided PDO connection
@@ -89,19 +99,19 @@
 				  -- TO-DO: Check if variable holding order is not empty. Make sure to replace null with your variable!
 				  -->
 				
-				<?php if (!empty(null)): ?>
+				<?php if (!empty($orderInfo)): ?>
 					<div class="order-details">
 
 						<!-- 
 				  		  -- TO DO: Fill in ALL the placeholders for this order from the db
   						  -->
 						<h1>Order Details</h1>
-						<p><strong>Name: </strong> <?= '' ?></p>
-				        	<p><strong>Username: </strong> <?= '' ?></p>
-				        	<p><strong>Order Number: </strong> <?= '' ?></p>
-				        	<p><strong>Quantity: </strong> <?= '' ?></p>
-				        	<p><strong>Date Ordered: </strong> <?= '' ?></p>
-				        	<p><strong>Delivery Date: </strong> <?= '' ?></p>
+						<p><strong>Name: </strong> <?= ($orderInfo['cname']) ?></p>
+				        	<p><strong>Username: </strong> <?= ($orderInfo['username']) ?></p>
+				        	<p><strong>Order Number: </strong> <?= ($orderInfo['ordernum']) ?></p>
+				        	<p><strong>Quantity: </strong> <?= ($orderInfo['quantity']) ?></p>
+				        	<p><strong>Date Ordered: </strong> <?= ($orderInfo['date_ordered']) ?></p>
+				        	<p><strong>Delivery Date: </strong> <?= ($orderInfo['date_deliv']) ?></p>
 				      
 					</div>
 				<?php endif; ?>
--- toy.php
+++ toy.php
@@ -17,7 +17,19 @@
 	 		  Retrieve info about toy from the db using provided PDO connection
 	 */
 
+	 function get_all_toy_info(PDO $pdo, string $toyId) {
+    $sql = "SELECT *
+            FROM toy
+            JOIN manuf ON toy.manid = manuf.manid
+            WHERE toy.toynum = :id;";
 
+    // Execute the SQL query using the pdo function and fetch the result
+	$toy = pdo($pdo, $sql, ['id' => $toyId])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+
+	// Return the toy information (associative array)
+	return $toy;
+}
+$toy_info = get_all_toy_info($pdo, $toy_id);
 // Closing PHP tag  ?> 
 
 <!DOCTYPE>
@@ -64,45 +76,46 @@
 			<div class="toy-details-container">
 				<div class="toy-image">
 					<!-- Display image of toy with its name as alt text -->
-					<img src="<?= '' ?>" alt="<?= '' ?>">
+					<img src="<?= $toy_info['imgSrc'] ?>" alt="<?= $toy_info['name'] ?>">
 				</div>
 
 				<div class="toy-details">
 
 					<!-- Display name of toy -->
-			        <h1><?= '' ?></h1>
+			        <h1><?= $toy_info['name'] ?></h1>
 
 			        <hr />
 
 			        <h3>Toy Information</h3>
 
 			        <!-- Display description of toy -->
-			        <p><strong>Description:</strong> <?= '' ?></p>
+			        <p><strong>Description:</strong> <?= $toy_info['description'] ?></p>
 
 			        <!-- Display price of toy -->
-			        <p><strong>Price:</strong> $ <?= '' ?></p>
+			        <p><strong>Price:</strong> $ <?= $toy_info['price'] ?></p>
 
 			        <!-- Display age range of toy -->
-			        <p><strong>Age Range:</strong> <?= '' ?></p>
+			        <p><strong>Age Range:</strong> <?= $toy_info['agerange'] ?></p>
 
 			        <!-- Display stock of toy -->
-			        <p><strong>Number In Stock:</strong> <?= '' ?></p>
+			        <p><strong>Number In Stock:</strong> <?= $toy_info['numinstock'] ?></p>
 
 			        <br />
 
 			        <h3>Manufacturer Information</h3>
 
 			        <!-- Display name of manufacturer -->
-			        <p><strong>Name:</strong> <?= '' ?> </p>
+			        <p><strong>Name:</strong> <?= $toy_info['name'] ?> </p>
 
 			        <!-- Display address of manufacturer -->
-			        <p><strong>Address:</strong> <?= '' ?></p>
+			        <p><strong>Address:</strong> <?= $toy_info['Street'] . ', ' . $toy_info['City'] 
+					. ', ' . $toy_info['State'] . ',' . $toy_info['ZipCode'] ?></p>
 
 			        <!-- Display phone of manufacturer -->
-			        <p><strong>Phone:</strong> <?= '' ?></p>
+			        <p><strong>Phone:</strong> <?= $toy_info['phone'] ?></p>
 
 			        <!-- Display contact of manufacturer -->
-			        <p><strong>Contact:</strong> <?= '' ?></p>
+			        <p><strong>Contact:</strong> <?= $toy_info['contact'] ?></p>
 			    </div>
 			</div>
 		</main>
